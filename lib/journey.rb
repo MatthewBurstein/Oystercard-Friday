d83cc84ea48a1091b928a799b7d99b71d309c7d7
Matt Burstein <j.matthew.burstein@gmail.com>
2018-02-01 16:24:45 +0000

    Complete Journey and Journey spec

--- lib/journey.rb
+++ lib/journey.rb
@@ -1,8 +1,25 @@
 class Journey
 
-  def initialize(entry_station, exit_station)
+  MIN_CHARGE = 3
+  FINE = 6
+
+  def initialize(entry_station = nil)
   @entry_station = entry_station
-  @exit_station = exit_station
   end
+
+  def fare
+    complete? ? FINE : MIN_CHARGE
+  end
+
+  def finish(station)
+    @exit_station = station
+  end
+
+  private
+
+  def complete?
+    !(@entry_station && @exit_station)
+  end
+
 
 end
--- spec/journey_spec.rb
+++ spec/journey_spec.rb
@@ -2,4 +2,43 @@ require 'journey'
 
 describe Journey do
 
+  let(:station) { double("a station") }
+  subject(:journey_with_entry) { described_class.new(station) }
+
+  describe "#fare" do
+
+    context "when journey has entry and exit stations" do
+      before :each do
+        journey_with_entry.finish(station)
+      end
+
+      it "charges the minimum charge" do
+        expect(journey_with_entry.fare).to eq Journey::MIN_CHARGE
+      end
+    end
+
+    context "When no exit station" do
+      it "returns the fine" do
+        expect(journey_with_entry.fare).to eq Journey::FINE
+      end
+    end
+
+    context "when no entry station" do
+      subject(:journey_without_entry) { described_class.new }
+      before :each do
+        journey_without_entry.finish(station)
+      end
+
+      it "charges the fine" do
+        expect(journey_without_entry.fare).to eq Journey::FINE
+      end
+    end
+  end
+
+  describe "#finish" do
+    it "sets @exit_station to the passed argument" do
+      expect(journey_with_entry.finish(station)).to eq station 
+    end
+  end
+
 end
